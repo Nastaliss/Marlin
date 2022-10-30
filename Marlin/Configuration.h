036b2582de54aec1d84c5b360baa412e37666f92
Stanislas Bruhiere <stanbill91@gmail.com>
2022-10-30 21:45:36 +0100

    Enable babystepping + change z offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 27.5, 0, -1.125 }
+#define NOZZLE_TO_PROBE_OFFSET { 27.5, 0, -0.6 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
