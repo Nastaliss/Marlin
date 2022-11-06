ae45c7bc11d287f7e885091f6d65b7528563497f
Stanislas Bruhiere <stanbill91@gmail.com>
2022-11-06 02:42:29 +0100

    Update steps per mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1026,7 +1026,7 @@
 // Mechanical endstop with COM to ground and NC to Signal uses "false" here (most common setup).
 #define X_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
 #define Y_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
-#define Z_MIN_ENDSTOP_INVERTING true // Set to true to invert the logic of the endstop.
+#define Z_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define I_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define J_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
 #define K_MIN_ENDSTOP_INVERTING false // Set to true to invert the logic of the endstop.
@@ -1084,7 +1084,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 400, 98.4 } // TODO: UPDATED ON REMOTE, MIGHT WANT TO TUNE THAT
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 98.75, 99, 396, 98.4 } // TODO: UPDATED ON REMOTE, MIGHT WANT TO TUNE THAT
 
 /**
  * Default Max Feed Rate (mm/s)
